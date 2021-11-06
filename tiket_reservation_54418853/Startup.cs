2c8a96e3244798358f1cf23b36e5c0c503e5b562
RayhanHamada <rayhanhamada1999@gmail.com>
2021-11-06 14:15:55 +0700

    Add project files.

---
+++ tiket_reservation_54418853/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tiket_reservation.Startup))]
+namespace tiket_reservation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
